90f20e5281148ba667047cffd275d4ee25a6aa13
adityamishra009 <adityamishra0905@gmail.com>
2025-07-05 22:00:56 +0530

    schema defined

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { dbConnection } from "./database/dbconnection.js";
 import { errorMiddleware } from './error/error.js';
+import reservationRouter from './routes/reservationRoute.js'
 
 const app = express();
 dotenv.config({path:"./config/config.env"});
@@ -15,6 +16,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
+
+app.use("/api/v1/reservation", reservationRouter)
 
 dbConnection();
 
